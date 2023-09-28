b1ba66055853f6b003f954ae00f0c356e6caee0c
Roberth <45379245+roberth1927@users.noreply.github.com>
2023-09-28 13:20:29 -0500

    delete comments

--- src/app/features/ejercicios/componentes/monedas/monedas.component.ts
+++ src/app/features/ejercicios/componentes/monedas/monedas.component.ts
@@ -15,49 +15,37 @@ export class MonedasComponent implements OnInit {
   encontrarMonedaMasPesada() {
     const monedas = ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9"];
 
-    // Primer pesaje (A y B vs C)
     const pesaje1 = this.compararPesos(
       [monedas[0], monedas[1]],
       [monedas[2], monedas[3]]
     );
 
     if (pesaje1 === 0) {
-      // El grupo C es más pesado
       const pesaje2 = this.compararPesos([monedas[4]], [monedas[5]]);
 
       if (pesaje2 === 0) {
-        // La moneda más pesada está en el grupo C
         this.resultado = "C";
       } else if (pesaje2 === 1) {
-        // La moneda más pesada está en el grupo E
         this.resultado = "E";
       } else {
-        // La moneda más pesada está en el grupo D
         this.resultado = "D";
       }
     } else if (pesaje1 === 1) {
-      // El grupo A es más pesado
       const pesaje3 = this.compararPesos([monedas[0]], [monedas[1]]);
 
       if (pesaje3 === 0) {
-        // La moneda más pesada está en el grupo B
         this.resultado = "B";
       } else {
-        // La moneda más pesada está en el grupo A
         this.resultado = "A";
       }
     } else {
-      // El grupo B es más pesado
       const pesaje4 = this.compararPesos([monedas[4]], [monedas[5]]);
 
       if (pesaje4 === 0) {
-        // La moneda más pesada está en el grupo F
         this.resultado = "F";
       } else if (pesaje4 === 1) {
-        // La moneda más pesada está en el grupo E
         this.resultado = "E";
       } else {
-        // La moneda más pesada está en el grupo D
         this.resultado = "D";
       }
     }
